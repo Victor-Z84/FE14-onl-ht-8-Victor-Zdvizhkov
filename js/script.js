b635112ae248fcfc01415c7e8313a36b23984bfc
Victor-Z84 <praguewedphoto@gmail.com>
2021-07-25 23:17:57 +0300

    landing 2

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
         var id = $(this).attr("href"),
             top = $(id).offset().top;
         $("body,html").animate({scrollTop: top}, 800);
-    })
+    });
 
 
     $(".clients-slider").slick({
